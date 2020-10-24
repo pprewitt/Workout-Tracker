6b8ff992cdfd7c926a9aed4f9fa834feea1ddc3f
Dr Phil <philip.prewitt@gmail.com>
2020-10-24 16:47:41 -0600

    deploying

--- server.js
+++ server.js
@@ -13,7 +13,14 @@ app.use(express.static('public'));
 
 const PORT = process.env.PORT || 4000;
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}
+);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
